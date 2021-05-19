1139c12f2535d1bffc146194cef17d19428431e8
Virginie <vbouvarel@lilo.org>
2021-05-19 11:24:26 +0200

    styles main

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 import Header from './components/Header';
+import TasksList from './components/Task/TasksList';
 
 function App() {
   return (
     <div className="App">
       <Header />
-
+      <main>
+        <TasksList />
+      </main>
     </div>
   );
 }
