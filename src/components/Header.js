20b5e298295745960daba58baadc9480dd102267
Virginie <vbouvarel@lilo.org>
2021-05-19 10:37:01 +0200

    ajout Header

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+//import React, { useState } from 'react';
+//import y from './y';
+//import y from '../UI/y';
+import classes from './Header.module.css';
+
+
+const Header = (props) => {
+  return (
+    <header className={classes.header}>
+      <h1>My ToDo</h1>
+    </header>
+  )
+}
+
+export default Header
