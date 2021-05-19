939604909a346bd13b51ff86a7f2db5115f2fd60
Virginie <vbouvarel@lilo.org>
2021-05-19 16:49:29 +0200

    suppression App.css

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
-import './App.css';
+import React, { useState } from 'react'
+
 import Header from './components/Header';
 import TasksList from './components/Task/TasksList';
 import TaskAddForm from './components/Task/TaskAddForm';
 
 function App() {
+  const [formIsVisible, setFormIsVisible] = useState(false);
+
+  const showFormHandler = () => {
+    setFormIsVisible(true);
+  }
+  const hideFormHandler = () => {
+    setFormIsVisible(false);
+  }
   return (
     <div className="App">
-      <TaskAddForm />
+      { formIsVisible && <TaskAddForm onClose={hideFormHandler} />}
       <Header />
       <main>
-        <TasksList />
+        <TasksList onPlus={showFormHandler} />
       </main>
     </div>
   );
