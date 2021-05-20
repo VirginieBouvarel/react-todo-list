b8fa6c97c0283a18f631569b081d5308c146eafe
Virginie <vbouvarel@lilo.org>
2021-05-20 12:32:34 +0200

    ajout édition des tâches

--- src/components/Task/TaskItem.js
+++ src/components/Task/TaskItem.js
@@ -7,7 +7,9 @@ import classes from './TaskItem.module.css';
 const TaskItem = (props) => {
   return (
     <li className={classes.item}>
-      <span>{props.label}</span>
+      <p>
+        <input className={classes.input} type="text" id={props.id} defaultValue={props.label} />
+      </p>
       <div className={classes.check} onClick={props.onCheck.bind(null, props.id)}></div>
     </li>
   );
